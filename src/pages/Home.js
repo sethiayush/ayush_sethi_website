a1ad74e21bdfdf96b49c70cfa040b42e06ed6a7c
sethiayush <66423558+sethiayush@users.noreply.github.com>
2026-04-29 20:38:17 +0530

    Create Home page component

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="dotted-grid"></div>
+      <div className="home-content">
+        {/* Content will go here */}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
